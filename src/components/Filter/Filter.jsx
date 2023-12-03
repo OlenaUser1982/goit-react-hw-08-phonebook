9369aaba8e527ca0eca203052589d3b1d220d8be
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-12-03 21:46:39 +0200

    1

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,9 +2,8 @@ const Filter = ({ filter, onChange }) => (
   <input
     type="text"
     value={filter}
-    onChange={e => onChange(e.target.value)}
+    onChange={e => onChange(e)}
     placeholder="Filter contacts by name:"
   />
 );
-
 export default Filter;
