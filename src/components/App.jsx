30e36fa25724598de32dc2d2cccc6abd28fd0b74
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-12-11 09:44:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
